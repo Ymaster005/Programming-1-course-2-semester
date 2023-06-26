e6edb9b67bbc1d3ccc4409bcd2d190394bc05d90
Markov Artem <113513575+Ymaster005@users.noreply.github.com>
2023-06-27 01:08:52 +0300

    Add files via upload

---
+++ Lab 6/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <locale>
+#include "Windows.h"
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	long double xlast, y, x0, interval;
+	
+	if (argc == 4) {
+		x0 = atof(argv[1]);
+		xlast = atof(argv[2]);
+		interval = atof(argv[3]);
+	}
+	
+	else {
+		cout << "Введіть через пробіл нижню границю, верхню границю та інтервал обчислень х " << endl;
+		cin >> x0 >> xlast >> interval;
+	}
+	
+	if (x0 > xlast || interval <= 0) {
+		cout << "Невірно введені значення х або інтервалу обчислень";
+		return 0;
+	}
+
+	while (x0 <= xlast) {
+
+		if (x0 > 8 && x0 <= 10)
+		{
+			y = ((1 / (2 + exp(-x0))) * log(sqrt(abs(2 - x0)) + 1.2)) + (cbrt(2 / x0));
+			cout << setw(10) << "x = " << x0 << setw(30) << "F(x) = " << y << endl;
+		}
+		else
+		{
+			if (x0 >= -5 && x0 <= 5)
+			{
+				if (x0 > -0.00001 && x0 < 0.00001) {
+					cout << setw(10) << "x = " << x0 << setw(30) << "F(x) = " << "Немає значення" << endl;
+				}
+				else {
+					y = pow(x0, -25);
+					cout << setw(10) << "x = " << x0 << setw(30) << "F(x) = " << y << endl;
+				}
+				
+			}
+
+			else
+			{
+				y = pow(x0, -100);
+				cout << setw(10) << "x = " << x0 << setw(30) << "F(x) = " << y << endl;
+			}
+		}
+
+		x0 = x0 + interval;
+
+	}
+	system("PAUSE");
+	return 0;
+}
---
+++ Lab 6/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <locale>
+#include "Windows.h"
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	
+	int next = 0;
+	double x = 1, sum = 0, item, eps = 1, iteration = 0;
+
+	std::cout << "Будь ласка введіть значення x (|x| < 1) " << endl;
+	cin >> x;
+	while (!(fabs(x) < 1)) {
+		std::cout << "Невірне значення аргументу" << endl;
+		cin >> x;
+	}
+	
+
+	item = ((log((1 + x) / (1 - x))) / 2);
+	
+	std::cout << "Значення функції = " << setprecision(8) << item << endl;
+
+
+	for (int i = 0; i < 5; i++) {
+		sum += (pow(x, ((2 * i) + 1)) / ((2 * i) + 1));
+		}
+
+	std::cout <<  "Сума перших 5 членів рядку = " << setprecision(8) << sum << endl;
+
+	std::cout << "Чи бажаєте ви розрахувати з точністю епсілон, якщо так, то введіть 1, ящо ні - 2" << endl;
+
+	while (!(next == 1 || next == 2)) {
+		cin >> next;
+	}
+
+	if (next == 1) {
+		std::cout << "Будь ласка введіть точність " << endl;
+		cin >> eps;
+		while (eps < 0.0000000000000001) {
+			std::cout << "епсілон занадто мале, будь ласка введіть інше значення" << endl;
+			cin >> eps;
+		}
+		
+	}
+
+	if (next == 2) {
+		return 0;
+	}
+
+	sum = 0;
+
+	for (int i = 0; fabs(item - sum) > eps; i++) {
+		sum += (pow(x, ((2 * i) + 1)) / ((2 * i) + 1));
+		iteration++;
+	}
+
+	std::cout << "Сума рядку = " << setprecision(16) << sum << "\t кількість ітеацій = " << iteration << endl;
+
+	system("PAUSE");
+	return 0;
+}
